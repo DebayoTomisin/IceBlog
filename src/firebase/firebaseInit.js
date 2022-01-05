c8e9c8e29016edb322f8e20aeee875509c4eb67e
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-05 03:19:14 -0800

    firebase setup and linking

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase, { initializeApp } from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQreDUfRTYaPLr3nz_iyHMQTmjh0aMB98",
+  authDomain: "iceblogs-275bf.firebaseapp.com",
+  projectId: "iceblogs-275bf",
+  storageBucket: "iceblogs-275bf.appspot.com",
+  messagingSenderId: "495949529166",
+  appId: "1:495949529166:web:e887c6ecc141980c4c031f"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const timeStamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { timeStamp };
+export default firebaseApp;
