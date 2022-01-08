bb7ab83d7c78044e8adea5965a6b5b9d17e0895e
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-08 07:18:27 -0800

    layout setting for blog cards component

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Blog card here</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'blogCard'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
