d114fdf20bb8061db35fcf0348fbe0d6fd3e6145
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-12 11:06:46 -0800

    login shenanigans

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Forgot Password'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,10 +6,10 @@
 
 <script>
     export default {
-        
+        name:'Login',
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 
 </style>
