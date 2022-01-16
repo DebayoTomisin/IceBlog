3fce8f4fab7d8019740ee6427cb1e63dbbcad272
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-16 00:22:57 -0800

    loading component

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="reset-password">
+        <Modal v-if="modalActive" v-on:close-modal="closeModal" />
         <div class="form-wrap">
         <form class="reset">
             <h2>Reset Password</h2>
@@ -20,10 +21,25 @@
 
 <script>
 import email from "../assets/Icons/envelope-regular.svg"
+import Modal from "../components/Modal.vue"
 export default {
     name: 'Forgot Password',
     components: {
-        email
+        email,
+        Modal
+    },
+    methods: {
+        closeModal() {
+            this.modalActive = !this.modalActive
+            this.email = ""
+        }
+    },
+    data () {
+        return {
+            email: null,
+            modalActive: null,
+            modalMessage: ""
+        }
     }
 }
 </script>
