898613ec38cad5c64d24bd97c51afd7decb56388
Debayo Tomisin <debayotomisin07@gmail.com>
2022-01-15 23:43:44 -0800

    reset password

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,15 +1,46 @@
 <template>
-    <div>
-
+    <div class="reset-password">
+        <div class="form-wrap">
+        <form class="reset">
+            <h2>Reset Password</h2>
+            <p>Forgot Password? Enter your email to reset it</p>
+            <div class="inputs">
+                <div class="input">
+                    <input type="text" placeholder="Email" v-model="email"/>
+                    <email class="icon" />
+                </div>
+            </div>
+            <button>Reset</button>
+            <div class="angle"></div>
+        </form>
+        <div class="background"></div>
+    </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Forgot Password'
+import email from "../assets/Icons/envelope-regular.svg"
+export default {
+    name: 'Forgot Password',
+    components: {
+        email
     }
+}
 </script>
 
 <style lang="scss" scoped>
-
+.reset-password {
+    position: relative;
+    .form-wrap {
+        .reset {
+            h2 {
+                margin-bottom: 8px;
+            }
+            p {
+                margin-bottom: 32px;
+                text-align: center;
+            }
+        }
+    }
+}
 </style>
