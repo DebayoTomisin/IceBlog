ca291787541c726e74f2c48441f815e03e0cdd1b
Debayo Tomisin <debayotomisin07@gmail.com>
2022-03-28 14:03:36 -0700

    Style/Implement styling for profile view on navigation bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -16,6 +16,31 @@
                     <div class="profile-menu">
                         <div class="info">
                             <p class='initials'>{{ this.$store.state.profileInitials }}</p>
+                            <div class="right">
+                                <p>{{ this.$store.state.profileFirstName }} {{ this.$store.state.profileLastName }}</p>
+                                <p> {{ this.$store.state.profileUserName }} </p>
+                                <p>{{ this.$store.state.profileEmail }}</p>                                
+                            </div>
+                        </div>
+                        <div class="options">
+                            <div class="option">
+                                <router-link to="#" class="option">
+                                    <userIcon class="icon" />
+                                    <p>Profile </p>
+                                </router-link>
+                            </div>
+                            <div class="option">
+                                <router-link to="#" class="option">
+                                    <adminIcon class="icon" />
+                                    <p>Admin</p>
+                                </router-link>
+                            </div>
+                            <div class="option">
+                                <router-link to="#" class="option">
+                                    <signOutIcon class="icon" />
+                                    <p>Sign out</p>
+                                </router-link>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -35,17 +60,24 @@
 
 <script>
 import menuIcon from '../assets/Icons/bars-regular.svg';
+import userIcon from '../assets/Icons/user-alt-light.svg';
+import adminIcon from '../assets/Icons/user-crown-light.svg';
+import signOutIcon from '../assets/Icons/sign-out-alt-regular.svg';
 
 export default {
     name: 'navigation',
     components: {
         menuIcon,
+        userIcon,
+        adminIcon,
+        signOutIcon
     },
     data () {
         return {
             mobile: null,
             mobileNav: null,
             windowWidth: null,
+            profileMenu: null
         }
     },
     created () {
@@ -119,6 +151,87 @@ header {
 
                 .link:last-child{
                     margin-right: 0;
+                }
+            }
+
+            .profile {
+                position: relative;
+                cursor: pointer;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                width:40px;
+                height: 40px;
+                border-radius: 50%;
+                color: #ffff;
+                background: #303030;
+
+                .profile-menu {
+                    position: absolute;
+                    top: 60px;
+                    right: 0;
+                    width: 250px;
+                    background-color: #303030;
+                    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+                    .info {
+                        display: flex;
+                        padding: 15px;
+                        border-bottom: 1px solid #fff;
+                        align-items: center;
+
+                        .initials {
+                            position: initial;
+                            width: 40px;
+                            height: 40px;
+                            background-color: #fff;
+                            color: #303030;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            border-radius: 50%;
+                        }
+
+                        .right {
+                            flex: 1;
+                            margin-left: 24px;
+
+                            p:nth-child(1) {
+                                font-size: 14px;
+                            }
+
+                            p:nth-child(2)
+                            p:nth-child(3) 
+                            {
+                                font-size: 12px;
+                            }
+                        }
+                    }
+
+                    .options {
+                        padding: 15px;
+
+                        .option {
+                            text-decoration: none;
+                            color: #fff;
+                            display: flex;
+                            margin-bottom: 12px;
+                            align-items: center;
+
+                            .icon {
+                                width: 18px;
+                                height: auto;
+                            }
+
+                            p {
+                                font-size: 14px;
+                                margin-left: 12px;
+                            }
+                            &:last-child {
+                                margin-bottom: 0pxs;
+                            }
+                        }
+                    }
                 }
             }
         }
